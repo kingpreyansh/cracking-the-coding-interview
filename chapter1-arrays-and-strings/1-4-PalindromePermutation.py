28789f856f761cff0f167bd369b66398d04aa262
kingpreyansh <preyanshdutta@gmail.com>
2022-01-26 07:42:10 -0500

    new questions

--- chapter1-arrays-and-strings/1-4-PalindromePermutation.py
+++ chapter1-arrays-and-strings/1-4-PalindromePermutation.py
@@ -1,1 +1,8 @@
-# Palindrome Permutation: Given a string, wrrite a function to check if it is a permuation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. You can ignore casing and non-letter characters.
+# Palindrome Permutation: Given a string, write a function to check if it is a permuation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. You can ignore casing and non-letter characters.
+
+# can you rearrange it to be a palindrome
+
+# ababa
+
+
+
